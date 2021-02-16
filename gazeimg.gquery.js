222c8e4a00e34d13ff5e9c81a148f98dd2671022
Ganxiaozhe <gxz@ganxiaozhe.com>
2021-02-16 20:46:07 +0800

    GazeImg v1.1 Release! 🌈
    GazeImg 灯箱支持图片分组展示，并可用 JS 直接调用。

--- gazeimg.gquery.js
+++ gazeimg.gquery.js
@@ -1,19 +1,28 @@
 // ==================================================
 // 
-// GazeImg.gQuery.js v1.0.1
+// GazeImg.gQuery.js v1.1.0
 // (c) 2020-present, JU Chengren (Ganxiaozhe)
 //
 // Licensed GPLv3 for open source use
 // or GazeImg Commercial License for commercial use
 //
+// gquery.net/plugins/gazeimg
+//
 // ==================================================
 ;(function($, window, document){
 	'use strict';
 	if(!$){throw new Error('GazeImg.js need gQuery: https://gquery.net/');}
-	console.log('%c GazeImg v1.0.1 %c www.gquery.net/plugins/gazeimg/ \n','color: #fff; background: #030307; padding:5px 0; margin-top: 1em;','background: #efefef; color: #333; padding:5px 0;');
+	console.log('%c GazeImg v1.1.0 %c www.gquery.net/plugins/gazeimg \n','color: #fff; background: #030307; padding:5px 0; margin-top: 1em;','background: #efefef; color: #333; padding:5px 0;');
 
 	let __gi = {
-		s: {close:0, move:0},
+		s: {close:0, move:0, queue:[], index:0},
+		fn: {
+			getTran: function($obj){
+				let tran = $obj.css('transform').replace(/[^0-9\-,.]/g,'').split(',');
+				tran.length!=6 && (tran = [0,0,0,0,0,0]);
+				return tran.map(v=>parseInt(v));
+			}
+		},
 		iloader: function(src){
 			return new Promise(function(resolve, reject){
 				const image = new Image();
@@ -25,77 +34,162 @@
 				if(image.complete){resolve(image);}
 			});
 		},
+		// 传入容器及图片原始长宽，返回在容器内的图片长宽等信息
+		// ww, wh, ow, oh | w, h, iz
+		isizeCalc: function(opts){
+			for(let k in opts){
+				isNaN(opts[k]) && ( opts[k] = parseInt(opts[k].replace('px','')) );
+			}
+			opts.w = opts.ow, opts.h = opts.oh;
+			// 是否放大
+			opts.iz = 0;
+			// 计算占比
+			opts.percW = opts.ow/opts.ww;
+			opts.percH = opts.oh/opts.wh;
+			// 如果双方都溢出，则取大的按100%计算
+			if(opts.percW>1 || opts.percH>1){
+				if(opts.percW > opts.percH){
+					opts.w = opts.ww;
+					opts.h = opts.w / opts.ow * opts.oh;
+				} else {
+					opts.h = opts.wh;
+					opts.w = opts.h / opts.oh * opts.ow;
+				}
+				opts.iz = 1;
+			}
+			return opts;
+		},
 		ishowBind: function(){
-			$('img[data-gishow]').removeAttr('data-gishow').addClass('gi-click').on('click', function(){
+			$('img[data-gishow]').each(function(idx){
 				let im = {$i:$(this)};
-				im.src = im.$i.attr('src');
-				__gi.show(im.src);
+				// 组别处理
+				im.group = im.$i.attr('data-gishow');
+				im.group && (im.$i.attr('data-gigp', im.group));
+
+				this.removeAttribute('data-gishow');
+				this.classList.add('gi-click');
+			}).on('click', function(){
+				let im = {i:this, $i:$(this), arr:[], idx:0};
+				im.arr.push( im.$i.attr('src') );
+
+				// 组别处理
+				im.group = im.$i.attr('data-gigp');
+				$(`img[data-gigp="${im.group}"`).each(function(idx){
+					// 清空数组
+					idx==0 && (im.arr.length=0);
+
+					im.arr.push( this.getAttribute('src') )
+					im.i == this && (im.idx = idx);
+				});
+
+				__gi.show(im.arr, im.idx);
 			});
 		},
-		show: function(src){
+		show: function(arr, idx){
 			let t = {};
+			idx || (idx=0);
+			__gi.s.queue = arr;
+			__gi.s.index = idx;
+
 			t.stage = "<div class='gazeimg-container'>"+
 				"<div class='gazeimg-bg'></div>"+
 				"<div class='gazeimg-inner'>"+
+					"<div class='gazeimg-nav'><div class='gazeimg-pages'>1 / 1</div></div>"+
 					"<div class='gazeimg-stage'>"+
-						"<div class='gazeimg-content'><div class='loader'></div></div>"+
+						"<div class='gazeimg-slide current'><div class='gazeimg-content'><div class='loader'></div></div></div>"+
 					"</div>"+
 				"</div>"+
 			"</div>";
 			$('body').append(t.stage).addClass('gi-nobar');
-			__gi.showBind();
+
+			__gi.sprepare();
+			__gi.sbind();
+			__gi.sloader($('.gazeimg-slide.current > .gazeimg-content'), arr[idx]);
+		},
+		sprepare: function(){
+			if(__gi.s.queue.length<2){return;}
+			let t = {$stage:$('.gazeimg-stage')}, queue = __gi.s.queue, index = __gi.s.index;
+
+			$('.gazeimg-pages').text(`${index+1} / ${queue.length}`);
+
+			$('.gazeimg-container').addClass('grabbing');
+			$('.gazeimg-slide:not(.current)').remove();
+			$('.gazeimg-slide.current').css({transform: 'translate(0px, 0px)'});
+
+			if(index > 0){
+				t.$stage.append("<div class='gazeimg-slide prev'><div class='gazeimg-content'><div class='loader'></div></div></div>");
+				__gi.sloader($('.gazeimg-slide.prev > .gazeimg-content'), queue[index-1]);
+			}
+			if(index < queue.length-1){
+				t.$stage.append("<div class='gazeimg-slide next'><div class='gazeimg-content'><div class='loader'></div></div></div>");
+				__gi.sloader($('.gazeimg-slide.next > .gazeimg-content'), queue[index+1]);
+			}
+
+			$('.gazeimg-container').removeClass('grabbing x-grabbing');
+		},
+		sloader: function($obj, src){
+			let t = {};
+			$obj.html("<div class='loader'></div>");
+
+			// 容器尺寸
+			t.$stage = $('.gazeimg-stage');
+			t.stageW = t.$stage.width();t.stageH = t.$stage.height();
 
 			__gi.iloader(src).then(function(img){
-				// 容器尺寸
-				t.$stage = $('.gazeimg-stage');
-				t.stageW = t.$stage.width();t.stageH = t.$stage.height();
-				// 最终尺寸
-				t.iW = img.width;t.iH = img.height;
-				// 是否放大
-				t.isZoom = 0;
-				// 计算占比
-				t.percW = img.width/t.stageW;t.percH = img.height/t.stageH;
-				// 如果双方都溢出，则取大的按100%计算
-				if(t.percW>1 || t.percH>1){
-					if(t.percW>t.percH){
-						t.iW = t.stageW;
-						t.iH = t.iW / img.width * t.iH;
-					} else {
-						t.iH = t.stageH;
-						t.iW = t.iH / img.height * t.iW;
-					}
-					t.isZoom = 1;
-				}
+				// 计算处理
+				let icalc = __gi.isizeCalc({
+					ww: t.stageW, wh: t.stageH,
+					ow: img.width, oh: img.height
+				});
 
-				t.cls = t.isZoom ? 'zoom-in' : 'zoom-out';
+				t.cls = icalc.iz ? 'zoom-in' : 'zoom-out';
 
-				$('.gazeimg-content').data({
-					iW:t.iW, iH:t.iH,
-					inW:img.width, inH:img.height,
-					isZoom: t.isZoom, zoom: 0
-				}).html(`<img src='${src}'/>`).css({width:`${t.iW}px`, height:`${t.iH}px`}).addClass(t.cls);
-				$('.gazeimg-content img').on('mousedown', function(e){e.preventDefault();});
+				$obj.data({
+					iW:icalc.w, iH:icalc.h,
+					inW:icalc.ow, inH:icalc.oh,
+					isZoom: icalc.iz, zoom: 0
+				}).html(`<img src='${src}'/>`).css({
+					width:`${icalc.w}px`, height:`${icalc.h}px`
+				}).addClass(t.cls);
+				$obj.find('img').on('mousedown', function(e){e.preventDefault();});
+			}).catch(function(err){
+				$obj.data({
+					isZoom:0, zoom:0
+				}).css({width:'auto'}).html(`<div class='dialog'>图片加载失败！</div>`);
 			});
 		},
-		showBind: function(){
+		sbind: function(){
+			// 仅需单次绑定的事件
 			$('.gazeimg-container').on('click', function(){
+				// 在移动时无效，防止和 mouse down\up 冲突
+				if(__gi.s.move>0){return;}
+
 				__gi.s.close++;
 				setTimeout(()=>{
 					__gi.s.close == 1 && __gi.showClose();
 				},50);
 			});
 
+			$('.gazeimg-container').on({
+				mousedown: __gi.slideMove,
+				mouseup: __gi.slideMoveEnd,
+				touchstart: __gi.slideMove,
+				touchend: __gi.slideMoveEnd
+			}, '.gazeimg-slide');
 
-			$('.gazeimg-content').on({
+			$('.gazeimg-container').on({
 				click: function(){
+					// 在移动时无效，防止和 mouse down\up 冲突
+					if(__gi.s.move>0){return;}
+
 					__gi.s.close--;
 					let t = {$obj: $(this)};
 
-					// 是否在缩放下查看
-					if(__gi.s.move>0){return;}
-
-					// 是否放大
-					if(t.$obj.data('isZoom')!=1){__gi.showClose();return;}
+					// 是否支持放大，否则关闭
+					if(t.$obj.data('isZoom')!=1){
+						__gi.showClose();
+						return;
+					}
 
 					if(t.$obj.data('zoom')==1){
 						// 放大状态下
@@ -114,18 +208,138 @@
 				mouseup: __gi.showMoveEnd,
 				touchstart: __gi.showMove,
 				touchend: __gi.showMoveEnd
+			}, '.gazeimg-content');
+		},
+		slideMove: function(e){
+			let $obj = $(this), pos = {};
+			if( (e.type=='mousedown' && e.which!=1) || $obj.find('.gazeimg-content').data('zoom')==1 ){
+				return;
+			}
+
+			pos.tran = __gi.fn.getTran($obj);
+			$('.gazeimg-container').addClass('grabbing');
+
+			pos.evt = (e.type=='touchstart' ? 'touchmove.drag' : 'mousemove.drag');
+			this.setCapture && this.setCapture();
+			$(document).on(pos.evt, function(de){
+				if(de.type=='touchmove'){
+					pos.L = pos.tran[4] + de.targetTouches[0].pageX - e.targetTouches[0].pageX;
+					pos.T = pos.tran[5] + de.targetTouches[0].pageY - e.targetTouches[0].pageY;
+				} else {
+					pos.L = pos.tran[4] + de.pageX - e.pageX;
+					pos.T = pos.tran[5] + de.pageY - e.pageY;
+				}
+
+				if(!pos.type){
+					__gi.s.move = 1;
+					pos.type = Math.abs(pos.T)>Math.abs(pos.L) ? 'y' : 'x';
+
+					// 当图片少于2张时，只能上下滑动
+					__gi.s.queue.length<2 && (pos.type = 'y');
+
+					pos.type=='x' && $('.gazeimg-container').addClass('x-grabbing');
+				}
+
+				if(pos.type=='y'){
+					// 上下滑退出
+					pos.opa = 1 - Math.abs(parseInt(pos.T))/300;
+					pos.opa < 0 && (pos.opa = 0);
+					$obj.css({transform: 'translate(0px,'+pos.T+'px)', opacity: pos.opa});
+				} else {
+					// 左右滑切换
+					let $prev = $('.gazeimg-slide.prev'), $next = $('.gazeimg-slide.next');
+					pos.ww = window.innerWidth;
+					pos.xperc = pos.L/pos.ww;
+
+					// 没有图片时
+					if(pos.xperc<0){
+						// next
+						if(__gi.s.index>=__gi.s.queue.length-1){pos.L/=4;pos.xperc=0;}
+					} else {
+						// prev
+						if(__gi.s.index<1){pos.L/=4;pos.xperc=0;}
+					}
+
+					$obj.css({transform: 'translate('+pos.L+'px,0px)'});
+
+					$prev.css({transform: 'translate('+(pos.L-pos.ww)+'px,0px)'});
+					$next.css({transform: 'translate('+(pos.L+pos.ww)+'px,0px)'});
+				}
+
+				__gi.s.slidePos = pos;
 			});
+			__gi.s.slidePos = pos;
+		},
+		slideMoveEnd: function(e){
+			let pos = __gi.s.slidePos, $obj = $(this);
+			if($obj.find('.gazeimg-content').data('zoom')==1){return;}
+
+			$('.gazeimg-container').removeClass('grabbing');
+			setTimeout(()=>{__gi.s.move = 0;}, 50);
+
+			this.releaseCapture && this.releaseCapture();
+			$(document).off(pos.evt);
+
+			// 上下滑退出
+			if(pos.type=='y'){
+				if(pos.opa<0.5){
+					__gi.showClose();
+				} else {
+					$obj.css({transform: 'translate(0px, 0px)', opacity:1});
+				}
+			}
+			if(pos.type!='x'){return;}
+
+
+			// 左右滑切换
+			let $prev = $('.gazeimg-slide.prev'), $next = $('.gazeimg-slide.next');
+
+			// 切换失败
+			if( Math.abs(pos.xperc)<0.2 ){
+				$prev.css({transform: `translate(${-pos.ww}px, 0px)`});
+				$obj.css({transform: 'translate(0px, 0px)'});
+				$next.css({transform: `translate(${pos.ww}px, 0px)`});
+
+				setTimeout(()=>{$('.gazeimg-container').removeClass('x-grabbing');}, 300);
+				return;
+			}
+
+			// 向右切换 next
+			if(pos.xperc<0){
+				__gi.s.index++;
+				$obj.css({transform: `translate(${-pos.ww}px, 0px)`});
+				$next.css({transform: `translate(0px, 0px)`});
+
+				setTimeout(()=>{
+					$obj.removeClass('current').addClass('prev');
+					$next.removeClass('next').addClass('current');
+					__gi.sprepare();
+				}, 300);
+				return;
+			}
+
+			// 向左切换 prev
+			__gi.s.index--;
+			$obj.css({transform: `translate(${pos.ww}px, 0px)`});
+			$prev.css({transform: `translate(0px, 0px)`});
+
+			setTimeout(()=>{
+				$obj.removeClass('current').addClass('next');
+				$prev.removeClass('prev').addClass('current');
+				__gi.sprepare();
+			}, 300);
 		},
 		showMove: function(e){
 			let $obj = $(this), pos = {};
+			if( (e.type=='mousedown' && e.which!=1) || $obj.data('zoom')==0 ){
+				return;
+			}
 
-			pos.tran = $obj.css('transform').replace(/[^0-9\-,.]/g,'').split(',');
-			pos.tran.length!=6 && (pos.tran = [0,0,0,0,0,0]);
-			pos.tran = pos.tran.map(v=>parseInt(v));
-
-			this.setCapture && this.setCapture();
-			pos.evt = (e.type=='touchstart' ? 'touchmove.drag' : 'mousemove.drag');
+			pos.tran = __gi.fn.getTran($obj);
 			$obj.addClass('grabbing');
+
+			pos.evt = (e.type=='touchstart' ? 'touchmove.drag' : 'mousemove.drag');
+			this.setCapture && this.setCapture();
 			$(document).on(pos.evt, function(de){
 				__gi.s.move = 1;
 
@@ -137,36 +351,22 @@
 					pos.T = pos.tran[5] + de.pageY - e.pageY;
 				}
 
-				if($obj.data('zoom')==1){
-					$obj.css({transform: 'translate('+pos.L+'px,'+pos.T+'px)'});
-				} else {
-					// 在缩小状态下，下滑退出
-					pos.opa = 1 - Math.abs(parseInt(pos.T))/300;
-					pos.opa < 0 && (pos.opa = 0);
-					$obj.css({transform: 'translate(0px,'+pos.T+'px)', opacity: pos.opa});
-				}
-				__gi.s.pos = pos;
+				$obj.css({transform: 'translate('+pos.L+'px,'+pos.T+'px)'});
 			});
 			__gi.s.pos = pos;
 		},
 		showMoveEnd: function(e){
 			let pos = __gi.s.pos, $obj = $(this);
+			if($obj.data('zoom')==0){return;}
+
 			$obj.removeClass('grabbing');
 			setTimeout(()=>{__gi.s.move = 0;}, 50);
 
 			this.releaseCapture && this.releaseCapture();
 			$(document).off(pos.evt);
-
-			// 在缩小状态下，下滑退出
-			if($obj.data('zoom')==0){
-				if(pos.opa<0.5){
-					__gi.showClose();
-				} else {
-					$obj.css({transform: 'translate(0px, 0px)', opacity:1});
-				}
-			}
 		},
 		showClose: function(){
+			$('.gazeimg-container').fadeOut(400);
 			$('.gazeimg-container .gazeimg-inner').addClass('gia-zoomOut');
 			$('.gazeimg-content').off();
 			setTimeout(()=>{
@@ -189,12 +389,30 @@
 
 			this.each(function(idx){
 				let si = {
-					$i:$(this), w:this.offsetWidth, h:this.offsetHeight, cls:this.className
+					$i:$(this), w:this.offsetWidth+'px', h:this.offsetHeight+'px', cls:this.className
 				};
+				si.$p = si.$i.parent();
 
 				si.src = si.$i.attr('data-gisrc');
 				si.$i.attr('data-gi-ready',1).removeAttr('data-gisrc');
 				opts.class && (si.cls = opts.class);
+
+				// 内置尺寸
+				si.de = si.src.split(' ');
+				if(si.de.length>1){
+					si.src = si.de[0];
+
+					si._de = si.de[1].split('=');
+					if(si._de[0]=='size'){
+						si._di = si._de[1].replace(/['"]/g,'').split(',');
+
+						si.calc = __gi.isizeCalc({
+							ww: si.$p.width(), wh: si.$p.height(),
+							ow: si._di[0], oh: si._di[1]
+						});
+						si.w = si.calc.w+'px', si.h = si.calc.h+'px';
+					}
+				}
 
 				// 是否为图片蒙版
 				si.isrc = si.$i.attr('src');
@@ -205,7 +423,7 @@
 
 				si.ohtml = this.outerHTML;
 				si.cls = si.cls ? ` class='${si.cls}'` : '';
-				si.style = ` style='width:${si.w}px;height:${si.h}px;background:${opts.bg[Math.floor(Math.random()*opts.bg.length)]}'`;
+				si.style = ` style='width:${si.w};height:${si.h};background:${opts.bg[Math.floor(Math.random()*opts.bg.length)]}'`;
 
 
 				si.html = `<div id='giQL-${idx}' data-gi-prepare${si.cls}${si.style}><div class='loader'></div></div>`;
@@ -224,17 +442,22 @@
 			t.ietop = this.getBoundingClientRect().top;
 			if(t.ietop >= t.deh){return true;}
 
-			let i = {$obj:$(this)};
+			let i = {$obj: $(this)};
 			i.src = i.$obj.removeAttr('data-gi-prepare').data('giSRC');
+			i.w = i.$obj.css('width'), i.h = i.$obj.css('height');
 
 			__gi.iloader(i.src).then(function(){
+				// 图片蒙版
 				if(i.$obj[0].tagName.toLowerCase()=='img'){
 					i.$obj.attr('src', i.src);__gi.ishowBind();return;
 				}
 
+				// HTML 蒙版
 				let html = i.$obj.data('giHTML');
 				i.$obj.ohtml(html);
-				$('img[data-gi-ready]').attr('src', i.src).removeAttr('data-gi-ready').addClass('gia-fadeIn');
+				$('img[data-gi-ready]').css({
+					width:i.w, height:i.h
+				}).attr('src', i.src).removeAttr('data-gi-ready').addClass('gia-fadeIn');
 				__gi.ishowBind();
 			}, function(){
 				let cls = i.$obj.addClass('gi-loading gi-failed').attr('class');
@@ -245,4 +468,13 @@
 
 	// 初始化
 	$('img[data-gisrc]').gazeimg();
+
+
+
+	// 暴露接口
+	$.extend({
+		gazeimg: {
+			show: __gi.show
+		}
+	});
 })(gQuery, window, document);
